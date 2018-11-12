e9986d437ddd54cdd45f1208dd5f2b417492b894
Gustavo Mendonca <gustavogmend@gmail.com>
2018-11-12 11:20:44 -0200

    Starting cloudera connection

--- cmd/metrics/main.go
+++ cmd/metrics/main.go
@@ -4,15 +4,17 @@ import (
 	"fmt"
 
 	"github.com/gmendonca/query-metrics-go/pkg/cloudera"
+	"github.com/spf13/viper"
 )
 
 func main() {
+	viper.AutomaticEnv()
 	c := &cloudera.Cloudera{
-		Host:     "cloudera.test.com",
-		Port:     7180,
-		Username: "username",
-		Password: "password",
-		SSL:      true,
+		Host:     viper.GetString("CLOUDERA_HOST"),
+		Port:     viper.GetInt("CLOUDERA_PORT"),
+		Username: viper.GetString("CLOUDERA_USERNAME"),
+		Password: viper.GetString("CLOUDERA_PASSWORD"),
+		SSL:      viper.GetBool("CLOUDERA_SSL"),
 	}
 
 	fmt.Println(c.GetURL())
--- pkg/cloudera/hive_metrics.go
+++ pkg/cloudera/hive_metrics.go
@@ -1,1 +1,3 @@
 package cloudera
+
+func GetMetrics()
