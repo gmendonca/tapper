3ebd23b1ee5afaa7b8393d061023a950fc7cffce
Gustavo Mendonca <gustavogmend@gmail.com>
2018-11-15 11:16:25 -0200

    Adding viper and cobra configuration

---
+++ cmd/root.go
@@ -0,0 +1,64 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/gmendonca/query-metrics-go/pkg/cloudera"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var cfgFile string
+
+var rootCmd = &cobra.Command{
+	Use:   "metrics",
+	Short: "something",
+	Long:  `tl; dr;`,
+	Run: func(cmd *cobra.Command, args []string) {
+		c := &cloudera.Cloudera{
+			Host:     viper.GetString("cloudera.host"),
+			Port:     viper.GetInt("cloudera.port"),
+			Username: viper.GetString("cloudera.username"),
+			Password: viper.GetString("cloudera.password"),
+			SSL:      viper.GetBool("cloudera.ssl"),
+		}
+
+		fmt.Println(c.GetURL())
+
+		fmt.Println(c.GetHiveMetastoreOpenConnectionMetrics())
+	},
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file")
+}
+
+func initConfig() {
+	// Don't forget to read config either from cfgFile or from home directory!
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Search config in home directory with name ".cobra" (without extension).
+		viper.AddConfigPath("./configs")
+		viper.SetConfigName("config")
+	}
+
+	viper.SetConfigType("yaml")
+
+	viper.AutomaticEnv()
+
+	if err := viper.ReadInConfig(); err != nil {
+		fmt.Println("Can't read config:", err)
+		os.Exit(1)
+	}
+}
