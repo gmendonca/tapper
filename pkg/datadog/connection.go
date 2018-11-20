33076fbff349859fe096fa08267eb6fc55ec2aca
Gustavo Mendonca <gustavogmend@gmail.com>
2018-11-20 18:33:46 -0200

    Starting to add documentation

--- pkg/datadog/connection.go
+++ pkg/datadog/connection.go
@@ -4,11 +4,13 @@ import (
 	datadogClient "gopkg.in/zorkian/go-datadog-api.v2"
 )
 
+// Datadog Struct that holds APIKey and ApplicationID
 type Datadog struct {
 	APIKey        string
 	ApplicationID string
 }
 
+// GetClient a DataDog Client that make available to users to send metrics or interact with Datadog API
 func (datadog *Datadog) GetClient() *datadogClient.Client {
 	client := datadogClient.NewClient(datadog.APIKey, datadog.ApplicationID)
 	return client
--- pkg/datadog/send_metrics.go
+++ pkg/datadog/send_metrics.go
@@ -6,7 +6,8 @@ import (
 	datadogClient "gopkg.in/zorkian/go-datadog-api.v2"
 )
 
-//
+// PostMetrics posts a point to the Datadog API
+// Returns true or false depending on the point being successfuly posted or not
 func (datadog *Datadog) PostMetrics(metricName string, point float64, hostname string, metricType string, tags []string) (bool, error) {
 	timeNow := float64(time.Now().Unix())
 	dataPoint := datadogClient.DataPoint{&timeNow, &point}
