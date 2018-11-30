3ec3f2b8c80b67cf168da5924b2e1024428acbfb
Gustavo Mendonca <gustavogmend@gmail.com>
2018-11-30 12:49:56 -0200

    Adding simple and naive parse to analyze queries

--- pkg/query/parser.go
+++ pkg/query/parser.go
@@ -2,35 +2,85 @@ package query
 
 import "strings"
 
-//AnalyzeQueries gets the most used tables and most used joins to send to Datadog
-func AnalyzeQueries(query string) []string {
-	query = strings.ToUpper(query)
-	parts := strings.Fields(query)
+type AnalyzedQuery struct {
+	TablesNames []string
+	Joins       []string
+}
 
-	var blacklistedTables []string
+func contains(s []string, e string) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
+}
+
+//AnalyzeQueries gets the most used tables and most used joins to send to Datadog
+func AnalyzeQueries(query string) AnalyzedQuery {
+	joinWords := []string{
+		"LEFT", "left",
+		"RIGHT", "right",
+		"FULL", "full",
+		"INNER", "inner",
+		"OUTER", "outer",
+		"CROSS", "cross",
+		"JOIN", "join",
+	}
+
+	tokens := strings.Fields(query)
+
+	var joinToken string
 	var tableNames []string
 	var joins []string
 
+	joinStatement := false
 	fromStatement := false
 
-	for idx, part := range parts {
-		if fromStatement {
-			// Here we are sure that's a FROM statement starting
-			// Now we should check if this is not the beginning of a new query
-			if part != "(" && part != "(SELECT" {
-				//Now we should check if w
-				//Then this should be a table name
-				if !tableNames.Contains(part) {
-					tableNames = append(tableNames, part)
+	for idx, token := range tokens {
+		if joinStatement {
+			if contains(joinWords, token) {
+				joinToken = joinToken + " " + token
+			} else {
+				//The second table that we're doing the join with
+				joinToken = joinToken + " " + token
+				joinStatement = false
+				fromStatement = false
+
+				if !contains(tableNames, token) {
+					tableNames = append(tableNames, token)
+				}
+
+				if !contains(joins, joinToken) {
+					joins = append(joins, joinToken)
 				}
 			}
-		}
-
-		if part == "FROM" {
-			// Starting FROM Statment
+		} else if fromStatement {
+			//Here we are sure that this is a from Statement
+			if token != "(" && token != "(SELECT" && token != "(select" {
+				//Here we are sure that is a table name
+				if !contains(tableNames, token) {
+					tableNames = append(tableNames, token)
+				}
+				if idx+1 < len(tokens) && contains(joinWords, tokens[idx+1]) {
+					joinStatement = true
+					joinToken = token
+				} else {
+					fromStatement = false
+				}
+			} else {
+				//Since it's a new query we shouldn't care about it anymore
+				fromStatement = false
+			}
+		} else if token == "FROM" || token == "from" {
 			fromStatement = true
 		}
 	}
 
-	return parts
+	a := AnalyzedQuery{
+		TablesNames: tableNames,
+		Joins:       joins,
+	}
+
+	return a
 }
--- pkg/query/parser_test.go
+++ pkg/query/parser_test.go
@@ -6,6 +6,11 @@ import (
 	"github.com/gmendonca/tapper/pkg/query"
 )
 
+type QueryTest struct {
+	Query string
+	Name  string
+}
+
 const (
 	QueryTest1 = `SELECT column1,
 	FROM table1
@@ -15,15 +20,64 @@ const (
 	FROM table1 LEFT JOIN table2 ON table1.column2 = table2.column2
 	WHERE column1 in (value1, value2)`
 
-	QueryTest3 = `SELECT column1,\nFROM table1\nWHERE column1 in (value1, value2)`
+	QueryTest3 = `SELECT column1, FROM table1 WHERE column1 in (value1, value2)`
+
+	QueryTest4 = `SELECT * FROM table1 CROSS JOIN table2`
+
+	QueryTest5 = `SELECT * FROM table1 JOIN table2 ON table1.a = table2.a JOIN table3 ON table2.b = table3.b`
+
+	QueryTest6 = `WITH table_tmp as (
+		SELECT a, b FROM table1 WHERE a > 10
+	)
+
+	SELECT DISTINCT a
+	FROM table_tmp FULL OUTER JOIN table2
+	GROUP BY 1
+	`
 )
 
 func TestAnalyzeQueries(t *testing.T) {
-	var parts []string
-	parts = query.AnalyzeQueries(QueryTest1)
-	t.Log(parts)
-	parts = query.AnalyzeQueries(QueryTest2)
-	t.Log(parts)
-	parts = query.AnalyzeQueries(QueryTest3)
-	t.Log(parts)
+	var analyzedQuery query.AnalyzedQuery
+	analyzedQuery = query.AnalyzeQueries(QueryTest1)
+	if len(analyzedQuery.Joins) > 0 {
+		t.Errorf("That shouldn't be any Joins in this query: %s", analyzedQuery.Joins)
+	}
+	if len(analyzedQuery.TablesNames) != 1 || analyzedQuery.TablesNames[0] != "table1" {
+		t.Errorf("There are more tables than expected or the table doesn't match table1: %s", analyzedQuery.TablesNames[0])
+	}
+	analyzedQuery = query.AnalyzeQueries(QueryTest2)
+	if len(analyzedQuery.Joins) != 1 {
+		t.Errorf("That should be only one Join in this query: %s", analyzedQuery.Joins)
+	}
+	if len(analyzedQuery.TablesNames) != 2 || analyzedQuery.TablesNames[0] != "table1" || analyzedQuery.TablesNames[1] != "table2" {
+		t.Errorf("There are more tables than expected or the table doesn't match the expected names: %s", analyzedQuery.TablesNames)
+	}
+	analyzedQuery = query.AnalyzeQueries(QueryTest3)
+	if len(analyzedQuery.Joins) > 0 {
+		t.Errorf("That shouldn't be any Joins in this query: %s", analyzedQuery.Joins)
+	}
+	if len(analyzedQuery.TablesNames) != 1 || analyzedQuery.TablesNames[0] != "table1" {
+		t.Errorf("There are more tables than expected or the table doesn't match table1: %s", analyzedQuery.TablesNames[0])
+	}
+	analyzedQuery = query.AnalyzeQueries(QueryTest4)
+	if len(analyzedQuery.Joins) != 1 {
+		t.Errorf("That should be only one Join in this query: %s", analyzedQuery.Joins)
+	}
+	if len(analyzedQuery.TablesNames) != 2 || analyzedQuery.TablesNames[0] != "table1" || analyzedQuery.TablesNames[1] != "table2" {
+		t.Errorf("There are more tables than expected or the table doesn't match the expected names: %s", analyzedQuery.TablesNames)
+	}
+	analyzedQuery = query.AnalyzeQueries(QueryTest5)
+	if len(analyzedQuery.Joins) != 2 {
+		t.Logf("Since this is a very naive solution to parse queries, this is failing cause only show one join for this query: %s", analyzedQuery.Joins)
+	}
+	if len(analyzedQuery.TablesNames) != 3 || analyzedQuery.TablesNames[0] != "table1" || analyzedQuery.TablesNames[1] != "table2" || analyzedQuery.TablesNames[2] != "table3" {
+		t.Logf("Since this is a very naive solution to parse queries, this is failing cause only show two table names: %s", analyzedQuery.TablesNames)
+	}
+	analyzedQuery = query.AnalyzeQueries(QueryTest6)
+	if len(analyzedQuery.Joins) != 1 {
+		t.Errorf("That should be only one Join in this query: %s", analyzedQuery.Joins)
+	}
+	if len(analyzedQuery.TablesNames) != 3 || analyzedQuery.TablesNames[0] != "table1" || analyzedQuery.TablesNames[1] != "table_tmp" || analyzedQuery.TablesNames[2] != "table2" {
+		t.Errorf("There are more tables than expected or the table doesn't match the expected names: %s", analyzedQuery.TablesNames)
+	}
 }
